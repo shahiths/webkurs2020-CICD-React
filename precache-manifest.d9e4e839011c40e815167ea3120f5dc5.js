2d7d760a9e3c4421621cf1a7644e50a7a4d8bd1e
Shahitha Sothinathan <shahiths@stud.ntnu.no>
2020-10-21 17:00:58 +0200

    Updates

---
+++ precache-manifest.d9e4e839011c40e815167ea3120f5dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131fe8f7c66da000e6e7652b021e118c",
+    "url": "/shahiths/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/shahiths/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "d96dcf3bc1a07b364f68",
+    "url": "/shahiths/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/shahiths/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shahiths/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96dcf3bc1a07b364f68",
+    "url": "/shahiths/webkurs2020-CICD-React/static/js/main.9d44ce97.chunk.js"
+  },
+  {
+    "revision": "8e353897511fcb7635fc",
+    "url": "/shahiths/webkurs2020-CICD-React/static/js/runtime-main.bc8ce118.js"
+  }
+]);
