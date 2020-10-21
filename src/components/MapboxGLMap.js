c0e2d5c993c9faf2c6c0d7fc58be582693692b0e
Shahitha Sothinathan <shahiths@stud.ntnu.no>
2020-10-21 18:32:25 +0200

    hei7

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -21,7 +21,7 @@ const MapboxGLMap = () => {
         container: mapContainer.current,
         style: `mapbox://styles/mapbox/${backgroundLayerID}`,
         center: [10.727367, 59.917058],
-        zoom: 20,
+        zoom: 18,
       });
 
       map.on("load", () => {
