9ff2aa31e757843e2932f3449cf0cf1b88c67b22
Ettellerannet <Ettellerannet@epost.no>
2020-10-21 16:36:12 +0000

    Updates

--- precache-manifest.0c0c8d1f29b112f172b198a1a21d7ce8.js
+++ precache-manifest.0c0c8d1f29b112f172b198a1a21d7ce8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1e22575bbb824438970f071c4caa833",
+    "revision": "b63de2e916dfd742fd4333d74739c0b3",
     "url": "/webkurs2020-CICD-React/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
   },
   {
-    "revision": "ce1dfb8a50bc66dc422c",
+    "revision": "3cd548d8d7b32e913633",
     "url": "/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/webkurs2020-CICD-React/static/js/2.e4d219f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ce1dfb8a50bc66dc422c",
-    "url": "/webkurs2020-CICD-React/static/js/main.db9dfe19.chunk.js"
+    "revision": "3cd548d8d7b32e913633",
+    "url": "/webkurs2020-CICD-React/static/js/main.b3ac8735.chunk.js"
   },
   {
     "revision": "580e99ae8bc13b712167",
